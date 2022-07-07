3df599d0df0ac74ce36397f87864b71f5bc44e32
huarong <huarong.li@aloudata.com>
2022-07-07 19:18:26 +0800

    testCase

--- src/test/java/com/dy/cdqa/btest/BTestApplicationTests.java
+++ src/test/java/com/dy/cdqa/btest/BTestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.dy.cdqa.btest;
 
+import com.dy.cdqa.btest.utils.YunxiaoUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BTestApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbb");
     }
+
+
+    @Test
+    void contextLoads2() throws Exception {
+        assertEquals("a","c","value匹配");
+    }
+
+    @Test
+    void contextLoads3() throws Exception {
+        assertTrue(5>3);
+    }
+
+
 
 }
